c5cff2b03852b5c0c7983d3aac098f31d7e9285a
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-06-07 16:26:20 -0400

    Changed the button class

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,18 +3,14 @@ import "./style.css";
 export const Input = props => {
   return (
     <div className="form-group">
-      <input className="form-control" {...props} />
+      <input className="form-control " {...props} />
     </div>
   );
 };
 
 export const FormBtn = props => {
   return (
-    <button
-      {...props}
-      style={{ marginBottom: 10 }}
-      className="btn btn-outline-secondary"
-    >
+    <button {...props} style={{ marginBottom: 10 }} className="btn button">
       {props.children}
     </button>
   );
